507a25c57b5707faa8f2f05720d921f8b42e089d
Troy Rosenberg <tmr08c@gmail.com>
2023-03-10 13:06:30 -0500

    Add support for viewing a setlist
    Create a new LIveView, `Setlists.ShowLive` that supports fetching a
    setlist (based on Setlist.fm's ID) and displays information about the
    setlist, such as different "sets" that were played and the list of
    songs.

---
+++ lib/setlistify_web/live/setlists/show_live.ex
@@ -0,0 +1,41 @@
+defmodule SetlistifyWeb.Setlists.ShowLive do
+  use SetlistifyWeb, :live_view
+
+  alias Setlistify.SetlistFm
+
+  def mount(%{"id" => id}, _session, socket) do
+    if connected?(socket) do
+      {:ok, assign(socket, setlist: SetlistFm.API.get_setlist(id))}
+    else
+      {:ok, assign(socket, setlist: nil)}
+    end
+  end
+
+  def render(assigns) do
+    ~H"""
+    <%= if !@setlist do %>
+      Fetching setlist...
+    <% else %>
+      <%= @setlist.artist %> @ <%= @setlist.venue.name %> on <%= @setlist.date %>
+
+      <h2>Sets</h2>
+
+      <%= for set <- @setlist.sets do %>
+        <article>
+          <h2><%= set_name(set) %></h2>
+
+          <ol>
+            <%= for song <- set.songs do %>
+              <li><%= song %></li>
+            <% end %>
+          </ol>
+        </article>
+      <% end %>
+    <% end %>
+    """
+  end
+
+  defp set_name(%{encore: encore}) when is_number(encore), do: "Encore #{encore}"
+  defp set_name(%{name: nil}), do: "Unnamed Setlist"
+  defp set_name(%{name: name}), do: name
+end
