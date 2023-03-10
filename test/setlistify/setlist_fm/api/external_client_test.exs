ca182137cd165a1c24de8505f0ba640189bcaaa7
Troy Rosenberg <tmr08c@gmail.com>
2023-03-10 12:00:03 -0500

    Add support for fetching setlist from setlist.fm
    Add a new callback to our `SetlistFm.API` behavior, `get_setlist/1` to
    fetch a specific setlist by id.

--- test/setlistify/setlist_fm/api/external_client_test.exs
+++ test/setlistify/setlist_fm/api/external_client_test.exs
@@ -3,25 +3,19 @@ defmodule Setlistify.SetlistFm.API.ExternalClientTest do
 
   alias Setlistify.SetlistFm.API.ExternalClient
 
-  @response Path.join([
-              File.cwd!(),
-              "test",
-              "support",
-              "fixtures",
-              "setlist_fm_search_response.json"
-            ])
-            |> File.read!()
+  @fixture_dir Path.join([File.cwd!(), "test", "support", "fixtures"])
 
   setup do
     bypass = Bypass.open()
     {:ok, bypass: bypass}
   end
 
+  @search_response @fixture_dir |> Path.join("setlist_fm_search_response.json") |> File.read!()
   test "search/1", %{bypass: bypass} do
     Bypass.expect_once(bypass, "GET", "/search/setlists", fn conn ->
       conn
       |> Plug.Conn.put_resp_content_type("application/json")
-      |> Plug.Conn.resp(200, @response)
+      |> Plug.Conn.resp(200, @search_response)
     end)
 
     [event | _] = result = ExternalClient.search("modest mouse", endpoint_url(bypass.port))
@@ -30,6 +24,30 @@ defmodule Setlistify.SetlistFm.API.ExternalClientTest do
     assert event.artist == "Modest Mouse"
     assert event.venue.name == "9:30 Club"
     assert event.date == Date.new!(2022, 12, 20)
+    assert event.id
+  end
+
+  @get_response @fixture_dir |> Path.join("setlist_fm_setlist_response.json") |> File.read!()
+  test "get_setlist/1", %{bypass: bypass} do
+    id = Ecto.UUID.generate()
+
+    Bypass.expect_once(bypass, "GET", "/setlist/#{id}", fn conn ->
+      conn
+      |> Plug.Conn.put_resp_content_type("application/json")
+      |> Plug.Conn.resp(200, @get_response)
+    end)
+
+    result = ExternalClient.get_setlist(id, endpoint_url(bypass.port))
+
+    assert result.artist == "Modest Mouse"
+    assert result.venue.name == "Terminal 5"
+    assert result.date == Date.new!(2022, 12, 19)
+    assert length(result.sets) == 3
+    assert Enum.count(result.sets, & &1.encore) == 1
+
+    for %{songs: songs} <- result.sets do
+      assert length(songs) > 0
+    end
   end
 
   defp endpoint_url(port), do: "http://localhost:#{port}"
